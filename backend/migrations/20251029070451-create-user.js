836aeabc487bb128357511f36072d8afcc5aa5da
albert.lu <albert.lu@acercsi.com>
2025-10-29 18:21:57 +0800

    add migration

---
+++ backend/migrations/20251029070451-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      role: {
+        type: Sequelize.ENUM("normal", "admin"),
+        allowNull: false,
+        defaultValue: "normal"
+      },
+      oauth_type: {
+        type: Sequelize.ENUM("google", "line")
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
